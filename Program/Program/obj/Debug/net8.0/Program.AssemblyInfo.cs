2678c5b587d4c24e9a67b6d3df9a70ed46d92d2a
Katarzyna Mazurowska <mazurowska.kat@gmail.com>
2024-10-08 00:26:22 +0200

    Zrobiłam więcej lekcji z HTML Dodałam stronę z Cait i Vi jak się całują <3

--- Program/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c57e359e1825432dcbc872a545b55d87b0e5d37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+370b444db63e5d16c6ce2369a8ed7cdfc93c7009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
