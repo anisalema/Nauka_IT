afc197bec7ea944d2a80f9d05a0a148235b71b32
Katarzyna Mazurowska <mazurowska.kat@gmail.com>
2024-10-20 20:53:17 +0200

    Więcej CSS

--- Program/Program/obj/Release/net8.0/Program.AssemblyInfo.cs
+++ Program/Program/obj/Release/net8.0/Program.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa256ed2db0e2422a9d8aea3ed05a217db66258")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
